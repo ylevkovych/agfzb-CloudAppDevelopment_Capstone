65043170ffcb1f2c26201af14bfac4ab9c9265d0
ylevkovych <ylevkovych@gmail.com>
2023-05-10 14:47:27 -0400

    w3: functions.dealership added

---
+++ functions/dealership.js
@@ -0,0 +1,54 @@
+
+const { Cloudant } = require('@cloudant/cloudant'); 
+
+function main(params) {
+
+    cloudant_params = {
+        "URL": "https://b1f76e64-470e-4fa9-92ce-22c7f524bbc4-bluemix.cloudantnosqldb.appdomain.cloud",
+        "API_KEY": "9TTHdpmAE8OU2xEc4cu3xPswTWTyyEMUeUO2O1px9b_I"
+    };
+
+    return new Promise(function (resolve, reject) {
+        
+        const cloudant = Cloudant({
+            url: cloudant_params.URL,
+            plugins: {iamauth: {iamApiKey:cloudant_params.API_KEY}} 
+        });
+
+        const dbconn = cloudant.use('dealerships'); 
+        
+        if (params.state) { 
+            dbconn.find({"selector": {"state": {"$eq": params.state}}}, 
+                function (err, result) { 
+                    if (err) { 
+                        reject(err); 
+                    } 
+
+                    let code = result.docs.length==0 ? 404 : 200; 
+                    
+                    resolve({ 
+                        statusCode: code, 
+                        headers: {'Content-Type': 'application/json'}, 
+                        body: result 
+                    }); 
+                }
+            ); 
+        } else { 
+            dbconn.list({ include_docs: true }, 
+                function (err, result) { 
+                    if (err) { 
+                        reject(err); 
+                    } 
+
+                    let code = result.docs.length==0 ? 404 : 200; 
+
+                    resolve({ 
+                        statusCode: code, 
+                        headers: { 'Content-Type': 'application/json' }, 
+                        body: result 
+                    }); 
+                }
+            ); 
+        } 
+    });
+}
