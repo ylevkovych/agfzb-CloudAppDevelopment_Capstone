22e76ee4ff264bfd58dab4fbad4ebf8bc53a40d5
ylevkovych <ylevkovych@gmail.com>
2023-05-10 07:22:49 -0400

    w2: added loginlogout, signup functionalities

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,10 +16,13 @@ urlpatterns = [
     path(route='contact', view=views.contact, name='contact'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
